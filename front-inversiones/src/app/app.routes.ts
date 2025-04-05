6689e420209f445472de7560f9a8c81d1ac5d72a
Matias <matiasezequielsmania@gmail.com>
2025-04-05 15:35:39 -0300

    investments

--- front-inversiones/src/app/app.routes.ts
+++ front-inversiones/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { InvestmentsComponent } from './pages/investments/investments/investments.component';
 
 export const routes: Routes = [
     { path: 'login', component: AuthComponent },
     { path: 'home', component: HomeComponent },
+    { path: 'investments', component: InvestmentsComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: "**", redirectTo: "error", pathMatch: "full" },
 ];
