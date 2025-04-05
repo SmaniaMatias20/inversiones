4cf58b57f4d5d20b166359ef87cbbd4da9b60af6
Matias <matiasezequielsmania@gmail.com>
2025-04-05 10:19:25 -0300

    navegacion de login a home

--- front-inversiones/src/app/app.routes.ts
+++ front-inversiones/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: AuthComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: "**", redirectTo: "error", pathMatch: "full" },
+];
